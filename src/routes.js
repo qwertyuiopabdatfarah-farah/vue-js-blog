82b43edcd1a8900905f5ee271f5417858de4859a
Erwin GTI <erwin@gti.co.id>
2018-12-05 14:58:44 +0700

    tes

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import singleBlog from './components/singleBlog.vue'
 
 
 export default [
-  { path: '/', 		   name: 'data',   component:showBlog},
+  { path: '/', 		   name: 'data',   component:showBlog, alias: '/home'},
   { path: '/add', 	   name: 'add',    component:addBlog},
   { path: '/blog/:id', name: 'detail', component:singleBlog},
   { path: '*',         redirect: '/' },
